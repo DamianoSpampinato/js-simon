be07101684a6085a4b2687b05837b1216579fd61
DamianoSpampinato <ghgg585@gmail.com>
2024-03-19 20:10:37 +0100

    feat add point counter

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const randomNumberArray = [];
+const inputArray=[];
+let userNumber;
+let  randomNumber = 0;
+let points=0;
+while(randomNumberArray.length < 5){
+    randomNumber = Math.floor(Math.random() * 50) + 1;
+    if(!randomNumberArray.includes(randomNumber)){
+    randomNumberArray.push(randomNumber);
+    }
+}
+const myDiv =document.createElement('div');
+const  indexContainer =document.querySelector('#container');
+myDiv.innerHTML= (randomNumberArray);
+console.log(randomNumberArray);
+indexContainer.append(myDiv);
+
+setTimeout(removeNumbers, 3000);
+
+for(let i = 0; i<5; i++ ){
+    setTimeout(inputDelayed, 3100);
+}
+setTimeout(function(){alert('hai fatto ' + points)}, 4000)
+
+function removeNumbers(){
+    myDiv.innerHTML= ('');
+}
+function inputDelayed (){
+    userNumber = parseInt(prompt('inserisci il numero'));
+    if (randomNumberArray.includes(userNumber)){
+        points++
+    
+    }
+}
+console.log(inputArray);
